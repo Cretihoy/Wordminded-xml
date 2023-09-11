88df61572e0a0f479b5cc0f42ebc7e64a0d3807d
cretihoy <sergzhio238@gmail.com>
2023-09-11 18:40:04 +0300

    create question factory and add logic

---
+++ data/src/main/java/com/cretihoy/data/factory/QuestionFactory.kt
@@ -0,0 +1,125 @@
+package com.cretihoy.data.factory
+
+import com.cretihoy.data.R
+import com.cretihoy.data.model.LetterModel
+import com.cretihoy.data.model.TaskModel
+import javax.inject.Inject
+
+class QuestionFactory
+@Inject constructor() {
+
+    private var letterList = mutableListOf<LetterModel>()
+    private var taskList = mutableListOf<TaskModel>()
+
+    fun getRandomLetter(): LetterModel? {
+        val currentLetter = getLetterList().randomOrNull()
+        letterList.remove(currentLetter)
+        return currentLetter
+    }
+
+    fun getRandomTask(): TaskModel? {
+        val currentTask = getTaskList().randomOrNull()
+        taskList.remove(currentTask)
+        return currentTask
+    }
+
+    private fun getLetterList(): List<LetterModel> {
+        return listOf(
+            LetterModel(
+                letter = R.string.letter_01
+            ),
+            LetterModel(
+                letter = R.string.letter_02
+            ),
+            LetterModel(
+                letter = R.string.letter_03
+            ),
+//            R.string.letter_04,
+//            R.string.letter_05,
+//            R.string.letter_06,
+//            R.string.letter_07,
+//            R.string.letter_08,
+//            R.string.letter_09,
+//            R.string.letter_10,
+//            R.string.letter_11,
+//            R.string.letter_12,
+//            R.string.letter_13,
+//            R.string.letter_14,
+//            R.string.letter_15,
+//            R.string.letter_16,
+//            R.string.letter_17,
+//            R.string.letter_18,
+//            R.string.letter_19,
+//            R.string.letter_20,
+//            R.string.letter_21,
+//            R.string.letter_22,
+//            R.string.letter_23,
+//            R.string.letter_24,
+//            R.string.letter_25,
+//            R.string.letter_26
+        ).shuffled()
+    }
+
+    private fun getTaskList(): List<TaskModel> {
+        return listOf(
+            TaskModel(
+                task = R.string.task_01
+            ),
+            TaskModel(
+                task = R.string.task_02
+            ),
+            TaskModel(
+                task = R.string.task_03
+            ),
+//            R.string.task_04,
+//            R.string.task_05,
+//            R.string.task_06,
+//            R.string.task_07,
+//            R.string.task_08,
+//            R.string.task_09,
+//            R.string.task_10,
+//            R.string.task_11,
+//            R.string.task_12,
+//            R.string.task_13,
+//            R.string.task_14,
+//            R.string.task_15,
+//            R.string.task_16,
+//            R.string.task_17,
+//            R.string.task_18,
+//            R.string.task_19,
+//            R.string.task_20,
+//            R.string.task_21,
+//            R.string.task_22,
+//            R.string.task_23,
+//            R.string.task_24,
+//            R.string.task_25,
+//            R.string.task_26,
+//            R.string.task_27,
+//            R.string.task_28,
+//            R.string.task_29,
+//            R.string.task_30,
+//            R.string.task_31,
+//            R.string.task_32,
+//            R.string.task_33,
+//            R.string.task_34,
+//            R.string.task_35,
+//            R.string.task_36,
+//            R.string.task_37,
+//            R.string.task_38,
+//            R.string.task_39,
+//            R.string.task_40,
+//            R.string.task_41,
+//            R.string.task_42,
+//            R.string.task_43,
+//            R.string.task_44,
+//            R.string.task_45,
+//            R.string.task_46,
+//            R.string.task_47,
+//            R.string.task_48,
+//            R.string.task_49,
+//            R.string.task_50,
+//            R.string.task_51,
+//            R.string.task_52,
+        ).shuffled()
+    }
+}
